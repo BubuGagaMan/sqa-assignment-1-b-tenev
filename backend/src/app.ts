83cf15bad9e48671550f8c261f6a3e51e1c5a05c
BubuGagaMan <bojidar_tenev@yahoo.com>
2026-01-15 14:50:06 +0000

    create a module for building the app

---
+++ backend/src/app.ts
@@ -0,0 +1,47 @@
+import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+
+import AppDataSource from "./db/data-source.js";
+
+import { testErrorCode } from "./errors.js";
+
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import dotenv from "dotenv";
+import authenticate from "@middleware/authenticate.js";
+
+dotenv.config();
+
+export async function buildApp(opts = {}) {
+  const app = fastify(opts);
+
+  await AppDataSource.initialize();
+  app.decorate("db", AppDataSource);
+
+  app.get("/", async (_response, _reply) => {
+    return { hello: "world" };
+  });
+  app.get("/exampleError", async (_response, _reply) => {
+    throw new testErrorCode();
+  });
+  app.register(fastifyJwt, {
+    secret: (process.env.JWT_SECRET || "test") as string,
+  });
+
+  app.decorate(
+    "authenticate",
+    (async (req: FastifyRequest, reply: FastifyReply) => await authenticate(req))
+  );
+
+  // manually call the notfound error handler (can direct system to jump to this route)
+  app.get("/notfound", async (_request, reply) => {
+    reply.callNotFound();
+  });
+
+  app.setNotFoundHandler(async (request, reply) => {
+    reply.code(404);
+    return { error: "Not found" };
+  });
+
+  return app;
+}
